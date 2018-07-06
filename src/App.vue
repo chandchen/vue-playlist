409466ce3962bdbf39dc15ef2dc2174377d9a739
Chand Chen <changduchen@gmail.com>
2018-07-06 20:44:07 +0800

    Update project to practice vuejs components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <img src="./assets/logo.png"> -->
+    <app-header></app-header>
+    <users></users>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+import Users from './components/Users'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      title: 'Hello world!'
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld,
+    'users': Users,
+    'app-header': Header,
+    'app-footer': Footer,
   }
 }
 </script>
 
-<style>
-#app {
+<style scoped>
+/*#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}*/
+h1{
+  color: purple;
 }
 </style>
---
+++ src/components/Users.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="users">
+  <h1>Hello Users!</h1>
+    <ul>
+      <li v-for='user in users' v-on:click='user.show=!user.show'>
+        <h2>{{ user.name }}</h2>
+        <h3 v-show="user.show">{{ user.position}}</h3>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  data () {
+    return {
+      users: [
+        {name: 'Chand', position: 'Developer', show: false},
+        {name: 'Chand', position: 'Developer', show: false}
+      ]
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.users{
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+ul{
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+
+li{
+  flex-grow: 1;
+  flex-basis: 200px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+}
+</style>
